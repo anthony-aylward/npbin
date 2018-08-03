e3d07751896d7e0c49923877b4c32c3ed8ecc6e1
Anthony Aylward <aaylward@eng.ucsd.edu>
2018-08-03 13:34:43 -0700

    test

---
+++ R/color_palette.R
@@ -0,0 +1,19 @@
+#===============================================================================
+# color_palette.R
+#===============================================================================
+
+#' A color palette
+#'
+#' @return a list representing the color palette
+#' @export
+color_palette <- function() {
+  list(
+    orange = "#D55E00",
+    blue = "#0072B2",
+    pink = "#CC79A7",
+    teal = "#009E73",
+    gold = "#E69F00",
+    lightblue = "#56B4E9",
+    yellow = "#F0E442"
+  )
+}
