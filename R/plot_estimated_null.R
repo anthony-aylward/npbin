038dee634755cbea41974e639129cc62768c3f08
Anthony Aylward <aaylward@eng.ucsd.edu>
2018-08-03 22:56:37 -0700

    allow plotting two nulls

--- R/plot_estimated_null.R
+++ R/plot_estimated_null.R
@@ -4,14 +4,27 @@
 
 #' @title Plot an estimated null distribution
 #'
+#' @description optionally superimpose two null distributions
+#'
 #' @param p_hat Numeric. Vector of observed ref allele proportions.
-#' @param shape1 Numeric. First shape parameter of the null distribution.
-#' @param shape2 Numeric. Second shape parameter of the null distribution.
+#' @param shape1_shape2 Numeric. Shape parameters of the first null
+#'   distribution.
+#' @param shape3_shape4 Numeric. Shape parameters of the second null
+#'   distribution.
 #' @export
-plot_estimated_null <- function(p_hat, shape1, shape2) {
+plot_estimated_null <- function(p_hat, shape1_shape2, shape3_shape4 = NULL) {
   pal <- color_palette()
   null_x <- 1:128 / 129
-  null_y <- dbeta(null_x, shape1, shape2)
+  shape1 = shape1_shape2[[1]]
+  shape2 = shape1_shape2[[2]]
+  null_y1 <- dbeta(null_x, shape1, shape2)
+  if (!is.null(shape3_shape4)) {
+    shape3 = shape3_shape4[[1]]
+    shape4 = shape3_shape4[[2]]
+    null_y2 <- dbeta(null_x, shape3, shape4)
+  } else {
+    null_y2 <- c()
+  }
   hist(
     p_hat,
     plot = TRUE,
@@ -19,14 +32,14 @@ plot_estimated_null <- function(p_hat, shape1, shape2) {
     col = "lavenderblush",
     border = pal[["pink"]],
     lty = 2,
-    ylim = c(0, max(null_y)),
+    ylim = c(0, max(c(null_y1, null_y2))),
     xlab = "Fraction Ref Allele",
     main = ""
   )
   par(new = TRUE)
   plot(
     null_x,
-    null_y,
+    null_y1,
     type = "l",
     lty = 2,
     lwd = 2,
@@ -37,4 +50,20 @@ plot_estimated_null <- function(p_hat, shape1, shape2) {
     yaxt = "n",
     xaxt = "n"
   )
+  if (!is.null(shape3_shape4)) {
+    par(new = TRUE)
+    plot(
+      null_x,
+      null_y1,
+      type = "l",
+      lty = 2,
+      lwd = 2,
+      col = pal[["teal"]],
+      bty = "n",
+      ylab = "",
+      xlab = "",
+      yaxt = "n",
+      xaxt = "n"
+    )
+  }
 }
